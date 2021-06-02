07d13152403293f18dd528f7c8dd77b81df5b70d
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 19:08:04 -0700

    4th Fizzbuzz test
    Tests that FizzBuzz:
    Prints a series of numbers from 1 to x
    Replaces multiples of three with fizz
    replaces multiples of five with buzz
    replaces multiples of three and five with fizzbuzz

--- test_fizz.py
+++ test_fizz.py
@@ -5,4 +5,4 @@ import fizz
 class Tester(unittest.TestCase):
     def test_fizz2(self):
         result = fizz.buzz(15)
-        self.assertEqual(result,"Print 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizz ")
+        self.assertEqual(result,"Print 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz ")
