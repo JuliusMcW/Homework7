d173116539a838362d27757dac8d4b4fb2356d3b
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 18:01:29 -0700

    First test
    Input=1978 to test if input is  divisible by 4.

---
+++ test_leap.py
@@ -0,0 +1,8 @@
+import unittest
+import leap.py
+
+
+class Tester(unittest.TestCase):
+    def test_leap1(self):
+        result = Julius_McWilliams_hw1.leap(1978)
+        self.assertEqual(result,True)
