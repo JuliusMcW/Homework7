dbb04355a0b25a3808ef81d4dd66f3e05bedaea2
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 19:04:11 -0700

    3rd Fizzbuzz test
    Tests that fizzbuzz returns a string from 1 to x while replacing any numbers divisible by 5 with buzz except those divisible by 3 which are replaced by fizz

--- test_fizz.py
+++ test_fizz.py
@@ -3,6 +3,6 @@ import fizz
 
 
 class Tester(unittest.TestCase):
-    def test_fizz1(self):
+    def test_fizz2(self):
         result = fizz.buzz(15)
-        self.assertEqual(result,"Print 1 2 fizz 4 5 fizz 7 8 fizz 10 11 fizz 13 14 fizz ")
+        self.assertEqual(result,"Print 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizz ")
