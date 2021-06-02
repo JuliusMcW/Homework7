dadcc02b8b641c9d0962ff334d62d8fb9035b58f
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 18:38:46 -0700

    4th code update
    Passes 4th test of 2004 is a leap year

--- leap.py
+++ leap.py
@@ -2,16 +2,17 @@
 
 def leapy(a):
 	if a%4==0:
-		if a%400:
+		if a%400==0:
+			print('Yes,', a,   'is a leap year')
+			return True
+		elif a%100!=0:
 			print('Yes,', a,   'is a leap year')
 			return True
 		else:
 			print('No,', a,   'is not a leap year')
 			return False
-
-
+		
 	else:
 		print('No,', a,   'is not a leap year')
 		return False
-
 
