2585579d0b5d66de9561741daa5481e9a25152f6
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 18:30:52 -0700

    Third Test
    Tests if 2100 returns false

--- test_leap.py
+++ test_leap.py
@@ -9,3 +9,6 @@ class Tester(unittest.TestCase):
     def test_leap2(self):
         result = leap.leapy(2000)
         self.assertEqual(result,True)
+    def test_leap3(self):
+        result = leap.leapy(2100)
+        self.assertEqual(result,False)
