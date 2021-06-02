1fd9c2c7cd276615f8076026b8db26dadd6eb3d0
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-01 18:59:14 -0700

    Second Test Update
    Test that numbers divisible by 3 are replaced with fizz (old test deleted as it is incompatible)

--- test_fizz.py
+++ test_fizz.py
@@ -5,5 +5,4 @@ import fizz
 class Tester(unittest.TestCase):
     def test_fizz1(self):
         result = fizz.buzz(15)
-        self.assertEqual(result,"Print 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ")
-   
+        self.assertEqual(result,"Print 1 2 fizz 4 5 fizz 7 8 fizz 10 11 fizz 13 14 fizz ")
